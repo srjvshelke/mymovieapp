38e62fbe8102965a219a356788d06384ec5eab82
surajvshelke <116488745+srjvshelke@users.noreply.github.com>
2024-06-05 18:19:22 +0530

    pagination

--- src/Page/Trending.jsx
+++ src/Page/Trending.jsx
@@ -11,7 +11,7 @@ const Trending = () => {
         const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=3ad017c328b9b52cdd05862101d1cb27`);
         setdatafortrend(data.results)
         console.log(data.results);
-        console.log( [...Array(datafortrend.length / 5)])
+        console.log([...Array(datafortrend.length / 5)])
     };
     useEffect(() => {
         fetchTrending();
@@ -25,32 +25,31 @@ const Trending = () => {
             </div>
 
             {/* body */}
-            {/* <div className='Trending_body flex'>
-                {
-                    datafortrend.slice(0, 10).map((ele, index) => {
-                        return (
-                            <>
-                                <Card img={ele.poster_path} name={ele.title || ele.name} date={ele.release_date || ele.first_air_date} type={ele.media_type} />
-                            </>
-                        )
+            <div className="bodypagin">
+                <div className='Trending_body flex'>
+                    {
+                        datafortrend.slice(page*10-10, page*10).map((ele, index) => {
+                            return (
+                                <>
+                                    <Card img={ele.poster_path} name={ele.title || ele.name} date={ele.release_date || ele.first_air_date} type={ele.media_type} />
+                                </>
+                            )
 
-                    })
-                }
+                        })
+                    }
 
-            </div> */}
+                </div>
 
-            {/* footer */}
-            <div class="pagination">
-                <span className={page > 1 ? "" : "pagination__disable"}>◀</span>
-
-                {
-                   
-                    [...Array(datafortrend.length%6==0?datafortrend.length/6:Math.floor(datafortrend.length/6)+1)].map((ele, i) => {
-                        return <span key={i} className={page === i + 1 ? "pagination__selected" : ""} >{i + 1}</span>
-                    })
-                }
-
-                <span className={page < datafortrend.length / 6 ? "" : "pagination__disable"}>▶</span>
+                {/* footer */}
+                <div class="pagination">
+                    <span onClick={() => selectPageHandler(page + 1)} className={page > 1 ? "" : "pagination__disable"}>◀</span>
+                    {
+                        [...Array(datafortrend.length % 10 == 0 ? datafortrend.length / 10 : Math.floor(datafortrend.length / 10) + 1)].map((ele, i) => {
+                            return <span key={i} className={page === i + 1 ? "pagination__selected" : ""} >{i + 1}</span>
+                        })
+                    }
+                    <span onClick={() => selectPageHandler(page + 1) } className={page < datafortrend.length / 10 ? "" : "pagination__disable"}>▶</span>
+                </div>
             </div>
         </div>
 
